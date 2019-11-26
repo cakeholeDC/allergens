3a8dc7ecd2794081c769e9a5f04b807a7b10c16a
Kyle <kylepcole@gmail.com>
2019-11-26 11:04:32 -0500

    models setup and seed data working

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Allergy.destroy_all
+RecipeIngredient.destroy_all
+Recipe.destroy_all
+User.destroy_all
+Ingredient.destroy_all
+
+10.times do	
+	user = User.create(name: Faker::Name.name)
+	recipe = Recipe.create(name: Faker::Food.dish, description: Faker::Food.description, directions: "Make the food!", user_id: user.id)
+	ingredient = Ingredient.create(name: Faker::Food.ingredient)
+	Allergy.create(user_id: user.id, ingredient_id: ingredient.id)
+	RecipeIngredient.create(recipe_id: recipe.id, ingredient_id: ingredient.id)
+end

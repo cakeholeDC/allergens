a3b520822c119263b867f3d827cda6a89b4674e5
stvik <stvik2@gmail.com>
2019-11-26 12:58:30 -0500

    started recipes form

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,4 +4,34 @@ class RecipesController < ApplicationController
 	def index
 		@recipes = Recipe.all
 	end
+
+
+	def new
+		@recipe = Recipe.new
+	end
+
+	def create
+		user = User.find_or_create_by(name: strong_params[:user_name])
+
+		@recipe = user.recipes.build(strong_params)
+
+		@recipe.save!
+
+		redirect_to recipe_path(@recipe)
+	end
+
+	def update
+
+	end
+
+
+	private
+
+	def find_recipe
+		@recipe = Recipe.find(params[:id])
+	end
+
+	def strong_params
+		params.require(:recipe).permit(:name,:description,:directions, :user_name)
+	end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,14 @@ class Recipe < ApplicationRecord
 	belongs_to :user
 	has_many :recipe_ingredients
 	has_many :ingredients, through: :recipe_ingredients
+	accepts_nested_attributes_for :user
+
+
+	def user_name=(name)
+		self.user = User.find_or_create_by(name: name)
+	end
+
+	def user_name
+		self.user ? self.user.name : nil
+	end
 end
